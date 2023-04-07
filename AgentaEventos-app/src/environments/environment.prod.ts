aaae9ea8b85436622336589c5cfe5665688e0cce
Lynx Pardelle <lynxpardelle@lynxpardelle.com>
2023-04-07 00:00:01 -0600

    production fixes

--- AgentaEventos-app/src/environments/environment.prod.ts
+++ AgentaEventos-app/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  //api: 'https://agenda-eventos.lynxpardelle.com/api',
-  api: 'http://localhost:3669/api',
+  api: 'https://agenda-eventos.lynxpardelle.com/api',
   app: 'AEA',
 };
